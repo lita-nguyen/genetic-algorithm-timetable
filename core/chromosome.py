f50f09f3f511b4f54d4024afb1416126de691292
LitaNguyen <nguyenlenhatlinh512@gmail.com>
2025-07-13 20:25:33 +0700

    add crossover

--- core/chromosome.py
+++ core/chromosome.py
@@ -26,3 +26,13 @@ class Chromosome:
             selected.sort(key=lambda x: x[1], reverse=True)
             half = selected[:len(selected)//2]
         return half
+    
+    def crossover(parent1, parent2, crossover_rate = 0.9):
+        length = len(parent1)
+        child1 = parent1.copy()
+        child2 = parent2.copy()
+
+        for i in range(length):
+            if random.random() < crossover_rate:
+                child1[i], child2[i] = child2[i], child1[i]
+        return child1, child2
