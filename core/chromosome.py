dd6089e59931a57d1daabf0fd6922bd305417536
Nhật Linh <161929608+lita-nguyen@users.noreply.github.com>
2025-07-13 21:07:48 +0700

    Crossover (#5)
    * add def selection
    
    * add selection
    
    * add crossover

--- core/chromosome.py
+++ core/chromosome.py
@@ -26,6 +26,16 @@ class Chromosome:
             selected.sort(key=lambda x: x[1], reverse=True)
             half = selected[:len(selected)//2]
         return half
+    
+    def crossover(parent1, parent2, crossover_rate = 0.9):
+        length = len(parent1)
+        child1 = parent1.copy()
+        child2 = parent2.copy()
+
+        for i in range(length):
+            if random.random() < crossover_rate:
+                child1[i], child2[i] = child2[i], child1[i]
+        return child1, child2
 
     def binary_array(self, course_order):
         binary_str = ''.join(self.genes[course["Course"]] for course in course_order)
