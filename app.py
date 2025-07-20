2246c9ff6155b5040cf335866a0087091b1993a9
MikeYuen <nhatminh.150596@gmail.com>
2025-07-20 22:08:07 +0700

    chore: note

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from core.chromosome import Chromosome
-from utils.fitness import (
-    calculate_fitness,
-    calculate_popularity,
-    build_schedule
-)
+from utils.fitness import calculate_fitness, calculate_popularity, build_schedule
 from utils.loader import load_courses, load_students, DAY_NAMES
 from core.selection import selection
 from core.crossover import crossover
 from core.mutation import mutation
 import random
 
+
 class Solution:
-    def __init__(self, courses_file="./data/courses.csv", students_file="./data/students.csv"):
+    def __init__(
+        self, courses_file="./data/courses.csv", students_file="./data/students.csv"
+    ):
         self.courses = load_courses(courses_file)
         self.students = load_students(students_file)
         self.popularity = calculate_popularity(self.students)
@@ -29,25 +28,24 @@ class Solution:
         population = []
         for student in self.students:
             item = self.evaluate_student(student)
-            population.append(item) 
-        
-        selected1_population = selection(population)
+            # random
+            # calc fitness
+            population.append(item)
 
-        print('-----selection', len(selected1_population))
+        selected1_population = selection(population) # (gen, fit)[]
+
+        print("-----selection", len(selected1_population))
         for item in selected1_population:
             print(item)
 
         children = []
         for _ in range(len(selected1_population) // 2):
-            # pick random 2
-            # chay crossover
-            # test
             parent_items = random.sample(selected1_population, 2)
-            # parent_items = [(gene[], fit), (gene2[], fit2)]
             gene, fit = parent_items[0]
             gene2, fit2 = parent_items[1]
 
             child1, child2 = crossover(gene, gene2)
+            # calc fit
             fit_child1 = self.evaluate_student(child1)
             fit_child2 = self.evaluate_student(child2)
 
@@ -59,17 +57,17 @@ class Solution:
             print("Child1: ", fit_child1)
             print("Child2: ", fit_child2)
 
+        print("-----crossover", len(children))
         
-        print('-----crossover', len(children))
-
+        # ???
         population2 = []
         for student2 in children:
             item2 = self.evaluate_student(student2)
-            population2.append(item2) 
-        
+            population2.append(item2)
+
         selected2_population = selection(population2)
 
-        print('-----selection2', len(selected2_population))
+        print("-----selection2", len(selected2_population))
         for item2 in selected2_population:
             print(item2)
         print("mutation")
@@ -78,13 +76,22 @@ class Solution:
             gene4 = mutation(gene3)
             fit4 = self.evaluate_student(gene4)
             population3.append((fit4))
-            print(fit4)
-        print('-----mutation', len(population3))
-            # mutated1 = mutation(child1)
-            # mutated2 = mutation(child2)
+            print('test', gene3, gene4, fit4)
+        print("-----mutation", len(population3)) # (gene, fit)[]
+        # mutated1 = mutation(child1)
+        # mutated2 = mutation(child2)
         # print(mutated1, mutated2)
         win = max(population3, key=lambda x: x[1])
         print("win: ", win)
+        # post-processing gene ==> timetable data
+        # [
+        #     ["Math", "Physics", "Chemistry", "Biology", "English", "", ""],
+        #     ["", "PE", "Math", "Physics", "", "", ""],
+        #     ["Literature", "", "Music", "", "History", "", ""],
+        #     ["", "", "", "", "", "", ""],
+        #     ["Art", "", "", "", "Math", "", ""],
+        # ]
+
 if __name__ == "__main__":
     solution = Solution()
     solution.process()
