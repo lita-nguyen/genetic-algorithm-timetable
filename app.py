f6a8a570da177c30197a81cd5431537e3761e67c
LitaNguyen <nguyenlenhatlinh512@gmail.com>
2025-07-20 18:49:28 +0700

    add app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from utils.fitness import (
     calculate_popularity,
     build_schedule
 )
-from utils.loader import load_courses, load_students, DAY_NAMES
+from utils.loader import load_courses, load_students
 from core.selection import selection
 from core.crossover import crossover
 from core.mutation import mutation
@@ -15,46 +15,77 @@ class Solution:
         self.courses = load_courses(courses_file)
         self.students = load_students(students_file)
         self.popularity = calculate_popularity(self.students)
+        self.threshold = 400
 
-    def evaluate_student(self, student):
-        chromosome = Chromosome(self.courses)
-        schedule = build_schedule(chromosome, self.courses)
+    def evaluate(self, chromo):
+        schedule = build_schedule(chromo, self.courses)
         reward, penalty = calculate_fitness(schedule, self.popularity)
-        total_score = reward + penalty
-
-        genes_binary = chromosome.binary_array(self.courses)
-        return (genes_binary, total_score)
+        score = reward + penalty
+        binary = chromo.binary_array(self.courses)
+        return (chromo, binary, score)
 
     def process(self):
-        population = []
-        for student in self.students:
-            item = self.evaluate_student(student)
-            population.append(item) 
-        
-        selected1_population = selection(population)
+        pop = [self.evaluate(Chromosome(self.courses)) for _ in range(len(self.students))]
+        gen = 0
+        best = max(pop, key=lambda x: x[2])
 
-        for _ in range(len(selected1_population)):
-            # pick random 2
-            # chay crossover
-            # test
-            parent_items = random.sample(selected1_population, 2)
-            # parent_items = [(gene[], fit), (gene2[], fit2)]
-            gene, fit = parent_items[0]
-            gene2, fit2 = parent_items[1]
-            child1, child2 = crossover(gene, gene2)
-            mutated1 = mutation(child1)
-            mutated2 = mutation(child2)
-        print(gene, fit)
-        print(gene2, fit2)
-        print(child1, child2)
-        print(mutated1, mutated2)
-        
+        while best[2] < self.threshold:
+            gen += 1
+            selected = selection(pop)
 
+            children = []
+            while len(children) < len(selected):
+                p1, _, _ = random.choice(selected)
+                p2, _, _ = random.choice(selected)
 
-        # print('-----', len(test))
-        # for item in test:
-        #     print(item)
+                g1, g2 = crossover(
+                    p1.binary_array(self.courses),
+                    p2.binary_array(self.courses)
+                )
+
+                for g in [g1, g2]:
+                    if len(children) >= len(selected):
+                        break
+                    child = Chromosome(self.courses)
+                    bits = ''.join(map(str, g))
+                    idx = 0
+                    for c in self.courses:
+                        child.genes[c["Course"]] = bits[idx:idx+3]
+                        idx += 3
+                    children.append(self.evaluate(child))
+
+            next_gen = selection(children)
+            if len(next_gen) != 10:
+                raise Exception("Expected 10 individuals after offspring selection")
+
+            mutated = []
+            for chromo, binary, _ in next_gen:
+                m_genes = mutation(binary)
+                mutated_chromo = Chromosome(self.courses)
+                bits = ''.join(map(str, m_genes))
+                idx = 0
+                for c in self.courses:
+                    mutated_chromo.genes[c["Course"]] = bits[idx:idx+3]
+                    idx += 3
+                mutated.append(self.evaluate(mutated_chromo))
+
+            pop = mutated
+            current = max(pop, key=lambda x: x[2])
+            if current[2] > best[2]:
+                best = current
+
+            print("Best fitness:", best[2])
+
+        print("Fitness:", best[2])
+        print("Gene:", best[1])
+
+        best_chromo = best[0]
+        schedule = build_schedule(best_chromo, self.courses)
+
+        print("\nFinal Schedule:")
+        day_names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        for course, (day, slot) in schedule.items():
+            print(f"{course}: {day_names[day]} {slot + 1}")
 
 if __name__ == "__main__":
-    solution = Solution()
-    solution.process()
+    Solution().process()
